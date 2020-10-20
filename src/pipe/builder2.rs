d1eb6116a62ed30d6a57d06137bd1b19f72f1c64
Mingwei Samuel <mingwei.samuel@gmail.com>
2020-10-19 22:31:56 -0700

    holy compiler

--- src/pipe/builder2.rs
+++ src/pipe/builder2.rs
@@ -4,32 +4,39 @@ pub struct Builder<A> {
     prev_builder: A,
 }
 impl <A> Builder<A> {
-    pub fn new() -> Self {
-        Builder {
-            value: (),
-        }
-    }
+    // pub fn new() -> Self {
+    //     Builder {
+    //         value: (),
+    //     }
+    // }
 
-    pub fn map<T, F, P>() -> Builder<MapPipeBuilder>
+    // pub fn map<T, F, P>() -> Builder<MapPipeBuilder>
+    // where
+    //     F: Fn(T) -> <P as Pipe>::Item,
+    //     P: Pipe,
+    // {
+
+    // }
+}
+
+trait PipeBuilder {
+    fn connect<Q: Pipe>(pipe: Q) -> <Self as PipeBuilderGat<Q>>::Output
     where
-        F: Fn(T) -> <P as Pipe>::Item,
-        P: Pipe,
-    {
-
-    }
+        Self: PipeBuilderGat<Q>;
 }
 
-trait PipeBuilder<P: Pipe> {
-    type Item;
-
-    fn connect<Q>(pipe: Q) -> P
-    where
-        Q: Pipe<Item = Self::Item>;
+trait PipeBuilderGat<Q> {
+    type Output;
 }
 
-struct NoOpPipeBuilder<A> {
-    _phantom: std::marker::PhantomData<A>,
+impl <Q, F, A> PipeBuilderGat<Q> for MapPipeBuilder<F, A, <Q as Pipe>::Item>
+where
+    Q: Pipe,
+    F: Fn(A) -> <Q as Pipe>::Item,
+{
+    type Output = MapPipe<A, F, Q>;
 }
+
 
 struct MapPipeBuilder<F, A, B>
 where
@@ -38,32 +45,57 @@ where
     mapper: F,
     _phantom: std::marker::PhantomData<( A, B )>,
 }
-impl <F, A, P> PipeBuilder<MapPipe<A, F, P>> MapPipeBuilder
-where
-    F: Fn(A) -> B,
-    P: Pipe
-{
-    type Output = B;
-    fn connect<P: Pipe<Item = Self::Output>>(self, pipe: P) ->  MapPipe<A, F, P> {
-        MapPipe::new(pipe, self.mapper)
-    }
-}
+// impl <F, A, P> PipeBuilder<MapPipe<A, F, P>> MapPipeBuilder
+// where
+//     F: Fn(A) -> B,
+//     P: Pipe
+// {
+//     type Output = B;
+//     fn connect<P: Pipe<Item = Self::Output>>(self, pipe: P) -> MapPipe<A, F, P> {
+//         MapPipe::new(pipe, self.mapper)
+//     }
+// }
 
 
 
 
-struct FilterPipeBuilder<F, A>
-where
-    F: Fn(A) -> bool,
-{
-    filter: F,
-    _phantom: std::marker::PhantomData<A>,
-}
+// struct NoOpPipeBuilder<A> {
+//     _phantom: std::marker::PhantomData<A>,
+// }
+
+// struct MapPipeBuilder<F, A, B>
+// where
+//     F: Fn(A) -> B,
+// {
+//     mapper: F,
+//     _phantom: std::marker::PhantomData<( A, B )>,
+// }
+// impl <F, A, P> PipeBuilder<MapPipe<A, F, P>> MapPipeBuilder
+// where
+//     F: Fn(A) -> B,
+//     P: Pipe
+// {
+//     type Output = B;
+//     fn connect<P: Pipe<Item = Self::Output>>(self, pipe: P) -> MapPipe<A, F, P> {
+//         MapPipe::new(pipe, self.mapper)
+//     }
+// }
+
+
+
+
+// struct FilterPipeBuilder<F, A>
+// where
+//     F: Fn(A) -> bool,
+// {
+//     filter: F,
+//     _phantom: std::marker::PhantomData<A>,
+// }
 
 
 
 
 
-trait ConnectPipe<P: Pipe> {
-    fn connect<Q: Pipe>(next_pipe: Q) -> P;
-}
+// trait ConnectPipe<P: Pipe> {
+//     fn connect<Q: Pipe>(next_pipe: Q) -> P;
+// }
