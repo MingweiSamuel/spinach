6f527682e5863a472fb9eb65680f7d6ca820ad0c
Mingwei Samuel <mingwei.samuel@gmail.com>
2020-10-30 10:54:44 -0700

    first pass set traits

--- src/traits.rs
+++ src/traits.rs
@@ -23,195 +23,145 @@ pub trait SetRead<T> {
 }
 
 pub trait SetWrite<T> {
-    fn add(&mut self, item: T);
+    fn insert(&mut self, item: T);
 
     fn union_in(&mut self, other: Self);
 
     fn intersect_in(&mut self, other: Self);
 }
 
+macro_rules! setwrite_set_impl {
+    ( $type:ident, $bound:ident ) => {
+        impl <T> SetWrite<T> for $type<T>
+        where
+            T: Eq + $bound,
+        {
+            fn insert(&mut self, item: T) {
+                self.insert(item);
+            }
 
-impl <'a, T> SetRead<&'a T> for &'a BTreeSet<T>
-where
-    T: Eq + Ord,
-{
-    fn map<F, C>(&self) -> C
-    where
-        F: UnaryFunction<Domain = &'a T>,
-        C: FromIterator<<F as UnaryFunction>::Codomain> + SetRead<<F as UnaryFunction>::Codomain>
-    {
-        self.iter()
-            .map(|x| F::call(x))
-            .collect()
-    }
+            fn union_in(&mut self, other: Self) {
+                self.extend(other.into_iter());
+            }
 
-    fn union<C>(&self, other: &Self) -> C
-    where
-        C: FromIterator<&'a T>,
-    {
-        (*self).union(*other)
-            .collect()
-    }
-
-    fn intersect<C>(&self, other: &Self) -> C
-    where
-        C: FromIterator<&'a T>,
-    {
-        (*self).intersection(*other)
-            .collect()
-    }
+            fn intersect_in(&mut self, other: Self) {
+                // Not clean or efficient.
+                *self = other.into_iter()
+                    .filter(|x| self.contains(x))
+                    .collect();
+            }
+        }
+    };
 }
+setwrite_set_impl!(BTreeSet, Ord);
+setwrite_set_impl!(HashSet, Hash);
 
-impl <'a, K, V> SetRead<(&'a K, &'a V)> for &'a BTreeMap<K, V>
-where
-    K: Eq + Ord,
-{
-    fn map<F, C>(&self) -> C
-    where
-        F: UnaryFunction<Domain = (&'a K, &'a V)>,
-        C: FromIterator<<F as UnaryFunction>::Codomain> + SetRead<<F as UnaryFunction>::Codomain>
-    {
-        self.iter()
-            .map(|x| F::call(x))
-            .collect()
-    }
 
-    fn union<C>(&self, other: &Self) -> C
-    where
-        C: FromIterator<(&'a K, &'a V)>,
-    {
-        let other_new = (*other).iter().filter(|(k, _)| !(*self).contains_key(k));
-        (*self).iter()
-            .chain(other_new)
-            .collect()
-    }
+macro_rules! setwrite_map_impl {
+    ( $type:ident, $bound:ident ) => {
+        impl <K, V> SetWrite<(K, V)> for $type<K, V>
+        where
+            K: Eq + $bound,
+            V: Eq,
+        {
+            fn insert(&mut self, item: (K, V)) {
+                self.insert(item.0, item.1);
+            }
 
-    fn intersect<C>(&self, other: &Self) -> C
-    where
-        C: FromIterator<(&'a K, &'a V)>,
-    {
-        (*self).iter()
-            .filter(|(k, _)| (*other).contains_key(k))
-            .collect()
-    }
+            fn union_in(&mut self, other: Self) {
+                self.extend(other.into_iter());
+            }
+
+            fn intersect_in(&mut self, other: Self) {
+                for (k, other_val) in other.into_iter() {
+                    if self.get(&k) != Some(&other_val) {
+                        self.remove(&k);
+                    }
+                }
+            }
+        }
+    };
 }
+setwrite_map_impl!(BTreeMap, Ord);
+setwrite_map_impl!(HashMap, Hash);
 
 
 
 
+macro_rules! setread_set_impl {
+    ( $type:ident, $bound:ident ) => {
+        impl <'a, T> SetRead<&'a T> for &'a $type<T>
+        where
+            T: Eq + $bound,
+        {
+            fn map<F, C>(&self) -> C
+            where
+                F: UnaryFunction<Domain = &'a T>,
+                C: FromIterator<<F as UnaryFunction>::Codomain> + SetRead<<F as UnaryFunction>::Codomain>
+            {
+                self.iter()
+                    .map(|x| F::call(x))
+                    .collect()
+            }
 
+            fn union<C>(&self, other: &Self) -> C
+            where
+                C: FromIterator<&'a T>,
+            {
+                (*self).union(*other)
+                    .collect()
+            }
 
+            fn intersect<C>(&self, other: &Self) -> C
+            where
+                C: FromIterator<&'a T>,
+            {
+                (*self).intersection(*other)
+                    .collect()
+            }
+        }
+    };
+}
+setread_set_impl!(BTreeSet, Ord);
+setread_set_impl!(HashSet, Hash);
 
+macro_rules! setread_map_impl {
+    ( $type:ident, $bound:ident ) => {
+        impl <'a, K, V> SetRead<(&'a K, &'a V)> for &'a $type<K, V>
+        where
+            K: Eq + $bound,
+        {
+            fn map<F, C>(&self) -> C
+            where
+                F: UnaryFunction<Domain = (&'a K, &'a V)>,
+                C: FromIterator<<F as UnaryFunction>::Codomain> + SetRead<<F as UnaryFunction>::Codomain>
+            {
+                self.iter()
+                    .map(|x| F::call(x))
+                    .collect()
+            }
 
+            fn union<C>(&self, other: &Self) -> C
+            where
+                C: FromIterator<(&'a K, &'a V)>,
+            {
+                let other_new = (*other).iter().filter(|(k, _)| !(*self).contains_key(k));
+                (*self).iter()
+                    .chain(other_new)
+                    .collect()
+            }
 
-// impl <T> SetRead<T> for BTreeSet<T>
-// where
-//     T: Eq + Ord + Clone,
-// {
-//     fn map<F, C>(&self) -> C
-//     where
-//         F: UnaryFunction<Domain = T>,
-//         C: FromIterator<<F as UnaryFunction>::Codomain> + SetRead<<F as UnaryFunction>::Codomain>
-//     {
-//         self.iter()
-//             .map(|x| F::call(x))
-//             .collect()
-//     }
-
-//     fn union<C>(&self, other: &Self) -> C
-//     where
-//         T: Clone,
-//         C: FromIterator<T>,
-//     {
-//         self.union(other)
-//             .cloned()
-//             .collect()
-//     }
-
-//     fn intersect<C>(&self, other: &Self) -> C
-//     where
-//         T: Clone,
-//         C: FromIterator<T>,
-//     {
-//         self.intersection(other)
-//             .cloned()
-//             .collect()
-//     }
-// }
-
-// impl <T> SetRead<T> for HashSet<T>
-// where
-//     T: Eq + Hash,
-// {
-//     fn map<'a, F, C>(&'a self) -> C
-//     where
-//         T: 'a,
-//         F: UnaryFunction<Domain = &'a T>,
-//         C: FromIterator<<F as UnaryFunction>::Codomain> + SetRead<<F as UnaryFunction>::Codomain>
-//     {
-//         self.iter()
-//             .map(|x| F::call(x))
-//             .collect()
-//     }
-
-//     fn union<C>(&self, other: &Self) -> C
-//     where
-//         T: Clone,
-//         C: FromIterator<T>,
-//     {
-//         self.union(other)
-//             .cloned()
-//             .collect()
-//     }
-
-//     fn intersect<C>(&self, other: &Self) -> C
-//     where
-//         T: Clone,
-//         C: FromIterator<T>,
-//     {
-//         self.intersection(other)
-//             .cloned()
-//             .collect()
-//     }
-// }
-
-// impl <K, V> SetRead<(K, V)> for BTreeMap<K, V>
-// where
-//     T: Eq + Ord,
-// {
-//     fn map<'a, F, C>(&'a self) -> C
-//     where
-//         T: 'a,
-//         F: UnaryFunction<Domain = &'a (K, V)>,
-//         C: FromIterator<<F as UnaryFunction>::Codomain> + SetRead<<F as UnaryFunction>::Codomain>
-//     {
-//         self.iter()
-//             .map(|x| F::call(x))
-//             .collect()
-//     }
-
-//     fn union<C>(&self, other: &Self) -> C
-//     where
-//         (K, V): Clone,
-//         C: FromIterator<(K, V)>,
-//     {
-//         panic!();
-//         // self.union(other)
-//         //     .cloned()
-//         //     .collect()
-//     }
-
-//     fn intersect<C>(&self, other: &Self) -> C
-//     where
-//         (K, V): Clone,
-//         C: FromIterator<(K, V)>,
-//     {
-//         panic!();
-//         // self.intersection(other)
-//         //     .cloned()
-//         //     .collect()
-//     }
-// }
-
-
+            fn intersect<C>(&self, other: &Self) -> C
+            where
+                C: FromIterator<(&'a K, &'a V)>,
+            {
+                (*self).iter()
+                    .filter(|(k, _)| (*other).contains_key(k))
+                    .collect()
+            }
+        }
+    };
+}
+setread_map_impl!(BTreeMap, Ord);
+setread_map_impl!(HashMap, Hash);
 
