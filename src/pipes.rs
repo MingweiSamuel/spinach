c73e607dd3fe1c2e7cfdbec46b9ac91597a8b0df
Mingwei Samuel <mingwei.samuel@gmail.com>
2020-11-16 14:16:39 -0800

    doesn't work

--- src/pipes.rs
+++ src/pipes.rs
@@ -7,158 +7,51 @@ use crate::merge::Merge;
 // use crate::semilattice::Semilattice;
 
 
-pub trait UnaryFn {
-    type Domain;
-    type Codomain;
-
-    fn call(input: Self::Domain) -> Self::Codomain;
-}
-
-
-
-pub trait Pipe {
+pub trait Pipe<'a> {
     type Input;
+    type Output;
 
-    fn push(&mut self, item: Self::Input) -> Result<(), &'static str>;
+    fn push(&'a mut self, item: Self::Input) -> Result<Self::Output, &'static str>;
 }
 
-
-
-pub struct Tank<F: Merge, P>
-where
-    P: Pipe<Input = F::Domain>
-{
-
+pub struct Tank<F: Merge> {
+    value: F::Domain,
 }
 
-
-
-pub struct Unary<F: UnaryFn, P>
+impl <'a, F: Merge> Pipe<'a> for Tank<F>
 where
-    P: Pipe<Input = F::Codomain>,
+    F::Domain: 'a,
 {
+    type Input = F::Domain;
+    type Output = &'a F::Domain;
+
+    fn push(&'a mut self, item: Self::Input) -> Result<Self::Output, &'static str> {
+        F::merge_in(&mut self.value, item);
+        Ok(&self.value)
+    }
+}
+
+pub struct PipeActor<'a, P: Pipe<'a>> {
     pipe: P,
-    _phantom: std::marker::PhantomData<F>,
+    receiver: mpsc::Receiver<P::Input>,
 }
-impl <F: UnaryFn, P> Unary<F, P>
-where
-    P: Pipe<Input = F::Codomain>,
-{
-    pub fn new(pipe: P) -> Self {
-        Self {
+impl <'a, P: Pipe<'a>> PipeActor<'a, P> {
+    pub fn create(pipe: P) -> ( mpsc::Sender<P::Input>, Self ) {
+        let ( sender, receiver ) = mpsc::channel(16);
+        let inst = Self {
             pipe: pipe,
-            _phantom: std::marker::PhantomData,
+            receiver: receiver,
+        };
+        ( sender, inst )
+    }
+
+    pub async fn run<'b: 'a>(&'b mut self) {
+        while let Some(item) = self.receiver.recv().await {
+            'a: {
+                let pipe = &'a mut self.pipe;
+                let value = pipe.push(item).expect("Failed to get value in PipeActor pipe.");
+            }
         }
     }
 }
-impl <F: UnaryFn, P> Pipe for Unary<F, P>
-where
-    P: Pipe<Input = F::Codomain>,
-{
-    type Input = F::Domain;
 
-    fn push(&mut self, item: Self::Input) -> Result<(), &'static str> {
-        self.pipe.push(F::call(item))
-    }
-}
-
-
-
-// pub trait Pipe {
-//     type Input;
-//     type Output;
-
-//     fn push(&mut self, item: Self::Input);
-
-//     fn get<'a>(&'a mut self) -> &'a Self::Output;
-// }
-
-// pub struct Tank<F: Merge> {
-//     value: F::Domain,
-// }
-// impl <F: Merge> Pipe for Tank<F> {
-//     type Input = F::Domain;
-//     type Output = F::Domain;
-
-//     fn push(&mut self, item: Self::Input) {
-//         F::merge_in(&mut self.value, item);
-//     }
-
-//     fn get<'a>(&'a mut self) -> &'a Self::Output {
-//         &self.value
-//     }
-// }
-
-// pub fn create_tank<F: Merge>() {
-
-// }
-
-// pub struct Tank<F: Merge> {
-//     value: F::Domain,
-//     sender: mpsc::Sender<F::Domain>,
-//     receiver: mpsc::Receiver<F::Domain>,
-// }
-
-// pub struct SenderPipe<F: Merge>(mpsc::Sender<F::Domain>);
-// impl <F: Merge> Pipe for SenderPipe<F> {
-//     type Input = F::Domain;
-
-//     fn push(&mut self, item: Self::Input) {
-//         self.0.send(item);
-//     }
-// }
-
-
-// pub struct Tank<F: Merge> {
-//     value: F::Domain,
-//     sender: watch::Sender<F::Domain>,
-// }
-
-// impl <F: Merge> Pipe for Tank<F>
-// where
-//     F::Domain: Clone,
-// {
-//     type Input = F::Domain;
-
-//     fn push(&mut self, item: Self::Input) {
-//         F::merge_in(&mut self.value, item);
-//         self.sender.send(self.value.clone());
-
-//         while let Ok(sender) = self.queue.try_recv() {
-//             self.senders.push(sender);
-//         }
-//         for sender in &self.senders {
-//             sender.send(self.value.clone());
-//         }
-//     }
-// }
-
-
-
-// pub struct Worker<M, S>
-// where
-//     M: Merge,
-//     S: Stream<Item = M::Domain>,
-// {
-//     stream: S,
-//     value: M::Domain,
-//     _phantom: std::marker::PhantomData<M>,
-// }
-
-// impl <M, S> Worker<M, S>
-// where
-//     M: Merge,
-//     S: Stream<Item = M::Domain>,
-// {
-//     pub fn new(stream: S, value: M::Domain) -> Self {
-//         Self {
-//             stream: stream,
-//             value: value,
-//             _phantom: std::marker::PhantomData,
-//         }
-//     }
-
-//     async fn execute(&mut self) {
-
-//     }
-// }
