f37f5d42258bfc1bced3abdfdadaeab1410b2aea
Mingwei Samuel <mingwei.samuel@gmail.com>
2020-11-17 17:41:52 -0800

    Everything is Pipe

--- src/pipes.rs
+++ src/pipes.rs
@@ -10,39 +10,16 @@ use crate::merge::Merge;
 // use crate::semilattice::Semilattice;
 
 
-pub trait Pipe {
-    type Input;
-
+pub trait Pipe<T> {
     #[must_use]
-    fn push(&mut self, item: Self::Input) -> Result<(), &'static str>;
+    fn push(&mut self, item: T) -> Result<(), &'static str>;
 }
 
-
-pub struct DebugPipe<T: Debug> {
+pub struct DebugPipe<T: Debug, P: Pipe<T>> {
+    next_pipe: P,
     _phantom: std::marker::PhantomData<T>,
 }
-impl <T: Debug> DebugPipe<T> {
-    pub fn new() -> Self {
-        Self {
-            _phantom: std::marker::PhantomData,
-        }
-    }
-}
-impl <T: Debug> Pipe for DebugPipe<T> {
-    type Input = T;
-
-    fn push(&mut self, item: Self::Input) -> Result<(), &'static str> {
-        println!("{:#?}", item);
-        Ok(())
-    }
-}
-
-
-pub struct ClonePipe<'a, T: Clone, P: Pipe<Input = T>> {
-    next_pipe: P,
-    _phantom: std::marker::PhantomData<&'a T>,
-}
-impl <'a, T: Clone, P: Pipe<Input = T>> ClonePipe<'a, T, P> {
+impl <T: Debug, P: Pipe<T>> DebugPipe<T, P> {
     pub fn new(next_pipe: P) -> Self {
         Self {
             next_pipe: next_pipe,
@@ -50,63 +27,125 @@ impl <'a, T: Clone, P: Pipe<Input = T>> ClonePipe<'a, T, P> {
         }
     }
 }
-impl <'a, T: Clone, P: Pipe<Input = T>> Pipe for ClonePipe<'a, T, P> {
-    type Input = &'a T;
-
-    fn push(&mut self, item: Self::Input) -> Result<(), &'static str> {
-        self.next_pipe.push(item.clone())
+impl <T: Debug, P: Pipe<T>> Pipe<T> for DebugPipe<T, P> {
+    fn push(&mut self, item: T) -> Result<(), &'static str> {
+        println!("{:#?}", item);
+        self.next_pipe.push(item)
     }
 }
 
-
-pub struct LatticePipe<'a, F: Merge> {
-    value: &'a RefCell<F::Domain>,
-}
-impl <'a, F: Merge> LatticePipe<'a, F> {
-    pub fn new(value: &'a RefCell<F::Domain>) -> Self {
-        Self {
-            value: value,
-        }
-    }
-}
-impl <'a, F: Merge> Pipe for LatticePipe<'a, F> {
-    type Input = F::Domain;
-
-    fn push(&mut self, item: Self::Input) -> Result<(), &'static str> {
-        F::merge_in(&mut self.value.borrow_mut(), item);
+pub struct NullPipe;
+impl <T> Pipe<T> for NullPipe {
+    fn push(&mut self, _item: T) -> Result<(), &'static str> {
         Ok(())
     }
 }
 
-pub struct LatticePipe2<F: Merge, P>
-where
-    for <'a> P: Pipe<Input = &'a F::Domain>,
-{
+
+// impl DebugPipe {
+//     pub fn new() -> Self {
+//         Self
+//     }
+// }
+// impl <T: Debug> Pipe<T> for DebugPipe {
+//     fn push(&mut self, item: T) -> Result<(), &'static str> {
+//         println!("{:#?}", item);
+//         Ok(())
+//     }
+// }
+
+
+pub struct LatticePipe<F: Merge, P: for<'a> Pipe<&'a F::Domain>> {
     value: F::Domain,
     next_pipe: P,
 }
-impl <F: Merge, P> LatticePipe2<F, P>
-where
-    for <'a> P: Pipe<Input = &'a F::Domain>,
-{
-    pub fn new(value: F::Domain, next_pipe: P) -> Self {
+impl <F: Merge, P: for<'a> Pipe<&'a F::Domain>> LatticePipe<F, P> {
+    pub fn new(bottom: F::Domain, next_pipe: P) -> Self {
         Self {
-            value: value,
+            value: bottom,
             next_pipe: next_pipe,
         }
     }
 }
-impl <F: Merge, P> Pipe for LatticePipe2<F, P>
-where
-    for <'a> P: Pipe<Input = &'a F::Domain>,
-{
-    type Input = F::Domain;
-
-    fn push(&mut self, item: Self::Input) -> Result<(), &'static str> {
+impl <F: Merge, P: for<'a> Pipe<&'a F::Domain>> Pipe<F::Domain> for LatticePipe<F, P> {
+    fn push(&mut self, item: F::Domain) -> Result<(), &'static str> {
         F::merge_in(&mut self.value, item);
         self.next_pipe.push(&self.value)
     }
 }
+
+
+// pub struct ClonePipe<'a, T: Clone, P: Pipe<Input = T>> {
+//     next_pipe: P,
+//     _phantom: std::marker::PhantomData<&'a T>,
+// }
+// impl <'a, T: Clone, P: Pipe<Input = T>> ClonePipe<'a, T, P> {
+//     pub fn new(next_pipe: P) -> Self {
+//         Self {
+//             next_pipe: next_pipe,
+//             _phantom: std::marker::PhantomData,
+//         }
+//     }
+// }
+// impl <'a, T: Clone, P: Pipe<Input = T>> Pipe for ClonePipe<'a, T, P> {
+//     type Input = &'a T;
+
+//     fn push(&mut self, item: Self::Input) -> Result<(), &'static str> {
+//         self.next_pipe.push(item.clone())
+//     }
+// }
+
+
+// pub struct LatticePipe<'a, F: Merge> {
+//     value: &'a RefCell<F::Domain>,
+// }
+// impl <'a, F: Merge> LatticePipe<'a, F> {
+//     pub fn new(value: &'a RefCell<F::Domain>) -> Self {
+//         Self {
+//             value: value,
+//         }
+//     }
+// }
+// impl <'a, F: Merge> Pipe for LatticePipe<'a, F> {
+//     type Input = F::Domain;
+
+//     fn push(&mut self, item: Self::Input) -> Result<(), &'static str> {
+//         F::merge_in(&mut self.value.borrow_mut(), item);
+//         Ok(())
+//     }
+// }
+
+// pub struct LatticePipe2<F: Merge, P>
+// where
+//     for <'a> P: PipeLifetime<'a, F::Domain>,
+// {
+//     value: F::Domain,
+//     next_pipe: P,
+//     // _phantom: std::marker::PhantomData<&'a ()>,
+// }
+// impl <F: Merge, P> LatticePipe2<F, P>
+// where
+//     for <'a> P: PipeLifetime<'a, F::Domain>,
+// {
+//     pub fn new(value: F::Domain, next_pipe: P) -> Self {
+//         Self {
+//             value: value,
+//             next_pipe: next_pipe,
+//             // _phantom: std::marker::PhantomData,
+//         }
+//     }
+// }
+// impl <F: Merge, P> Pipe for LatticePipe2<F, P>
+// where
+//     for <'a> P: PipeLifetime<'a, F::Domain>,
+// {
+//     type Input = F::Domain;
+
+//     fn push(&mut self, item: Self::Input) -> Result<(), &'static str> {
+//         F::merge_in(&mut self.value, item);
+//         self.next_pipe.push(&self.value)
+//     }
+// }
 
 
 // pub struct AnnaWorker<F: Merge, P: Pipe<Input = F::Domain>> {
