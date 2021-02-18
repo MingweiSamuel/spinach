92f5adf93c684f0b3dcac415ee72d2d1af9dd67d
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-02-17 22:57:36 -0800

    Remove old ops

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,10 @@
 #![feature(drain_filter)]
 // #![feature(impl_trait_in_bindings)]
 #![feature(type_alias_impl_trait)]
+#![feature(btree_retain)]
 
 pub mod merge;
 
 pub mod op2;
-
-pub mod ops;
-
-pub mod pull;
 
 pub mod semilattice;
