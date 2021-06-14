7f239d38f3b7650705eaddb6d03d722a9c14010c
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-06-14 17:03:51 +0000

    Fix test_tcp

--- lib/tests/test_tcp.rs
+++ lib/tests/test_tcp.rs
@@ -1,3 +1,4 @@
+use bytes::{BufMut, Bytes, BytesMut};
 use tokio::net::{TcpListener, TcpStream};
 
 use spinach::collections::Single;
@@ -11,10 +12,15 @@ use spinach::tag;
 
 struct Increment;
 impl Morphism for Increment {
-    type InLatRepr = SetUnionRepr<tag::VEC, usize>;
-    type OutLatRepr = SetUnionRepr<tag::VEC, usize>;
+    type InLatRepr = SetUnionRepr<tag::VEC, BytesMut>;
+    type OutLatRepr = SetUnionRepr<tag::VEC, Bytes>;
     fn call<Y: Qualifier>(&self, item: Hide<Y, Self::InLatRepr>) -> Hide<Y, Self::OutLatRepr> {
-        item.map(|x| x + 1)
+        item.map(|mut bytes| {
+            if let Some(rf) = bytes.get_mut(0) {
+                *rf += 1;
+            }
+            bytes.freeze()
+        })
     }
 }
 
@@ -28,9 +34,11 @@ pub async fn test_tcp() -> tokio::io::Result<()> {
     std::mem::drop(listener);
 
     let acceptor_op = TcpOp::new(acceptor.into_split().0);
-    let convert_op = BatchConvertOp::<_, SetUnionRepr<tag::VEC, usize>>::new(acceptor_op);
+    let convert_op = BatchConvertOp::<_, SetUnionRepr<tag::VEC, BytesMut>>::new(acceptor_op);
 
-    let once_op = OnceOp::<SetUnionRepr<tag::SINGLE, usize>>::new(Single(0));
+    let mut seed = BytesMut::with_capacity(1);
+    seed.put_u8(b'a');
+    let once_op = OnceOp::<SetUnionRepr<tag::SINGLE, BytesMut>>::new(Single(seed));
 
     let op = MergeOp::new(convert_op, once_op);
     let op = DebugOp::new(op, "test_tcp");
