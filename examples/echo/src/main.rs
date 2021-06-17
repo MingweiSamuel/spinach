762e74495a38a6fed077170979a47e71bd0fbfd6
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-06-16 18:20:34 -0700

    Use morphism_closure in echo example

--- examples/echo/src/main.rs
+++ examples/echo/src/main.rs
@@ -1,65 +1,13 @@
 #![feature(never_type)]
 
 use std::{env, process};
-use std::net::SocketAddr;
 
-use spinach::bytes::{Bytes, BytesMut};
 use spinach::tokio;
 use spinach::tokio::net::TcpStream;
 
 use spinach::comp::{CompExt};
-use spinach::func::unary::Morphism;
-use spinach::hide::{Hide, Qualifier};
-use spinach::lattice::setunion::SetUnionRepr;
 use spinach::op::{OpExt, ReadOp, TcpOp, TcpServerOp};
-use spinach::tag;
 use spinach::tcp_server::TcpServer;
-
-pub struct AddrBytesFreeze;
-impl Morphism for AddrBytesFreeze {
-    type InLatRepr  = SetUnionRepr<tag::SINGLE, (SocketAddr, BytesMut)>;
-    type OutLatRepr = SetUnionRepr<tag::SINGLE, (SocketAddr, Bytes)>;
-    fn call<Y: Qualifier>(&self, item: Hide<Y, Self::InLatRepr>) -> Hide<Y, Self::OutLatRepr> {
-        item.map_one(|(addr, bytes)| (addr, bytes.freeze()))
-    }
-}
-
-pub struct StringToBytes;
-impl Morphism for StringToBytes {
-    type InLatRepr  = SetUnionRepr<tag::SINGLE, String>;
-    type OutLatRepr = SetUnionRepr<tag::SINGLE, Bytes>;
-    fn call<Y: Qualifier>(&self, item: Hide<Y, Self::InLatRepr>) -> Hide<Y, Self::OutLatRepr> {
-        item.map_one(|string| string.into())
-    }
-}
-
-/// Entry point of the application.
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<!, String> {
-    // Begin by parsing the arguments. We are either a server or a client, and
-    // we need an address and potentially a sleep duration.
-    let args: Vec<_> = env::args().collect();
-
-    match &args[..] {
-        [_, mode, url]             if mode == "server" => server(url).await?,
-        [_, mode, url]             if mode == "server" => {
-            client(url, tokio::io::stdin()).await?
-        }
-        [_, mode, url, input_file] if mode == "client" => {
-            match tokio::fs::File::open(input_file).await {
-                Ok(file) => client(url, file).await?,
-                Err(err) => {
-                    eprintln!("Failed to open inpu_file: \"{}\", error: {}", input_file, err);
-                    process::exit(2);
-                }
-            }
-        }
-        _ => {
-            eprintln!("Usage:\n{0} server <url>\n  or\n{0} client <url> [input_file]", args[0]);
-            process::exit(1);
-        }
-    }
-}
 
 /// Run the server portion of the program.
 async fn server(url: &str) -> Result<!, String> {
@@ -68,7 +16,7 @@ async fn server(url: &str) -> Result<!, String> {
 
     TcpServerOp::new(server.clone())
         .debug("server")
-        .morphism(AddrBytesFreeze)
+        .morphism_closure(|item| item.map_one(|(addr, bytes)| (addr, bytes.freeze())))
         .comp_tcp_server(server)
         .run()
         .await
@@ -85,7 +33,7 @@ async fn client<R: tokio::io::AsyncRead + std::marker::Unpin>(url: &str, input_r
         .comp_debug("read");
 
     let write_comp = ReadOp::new(input_read)
-        .morphism(StringToBytes)
+        .morphism_closure(|item| item.map_one(|string| string.into()))
         .comp_tcp(write);
 
     let result = tokio::join!(
@@ -99,4 +47,32 @@ async fn client<R: tokio::io::AsyncRead + std::marker::Unpin>(url: &str, input_r
 
     Err(format!("Read error: {:?}, Write error: {:?}",
         result.0.unwrap_err(), result.1.unwrap_err()))
+}
+
+/// Entry point of the application.
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<!, String> {
+    // Begin by parsing the arguments. We are either a server or a client, and
+    // we need an address and potentially a sleep duration.
+    let args: Vec<_> = env::args().collect();
+
+    match &args[..] {
+        [_, mode, url]             if mode == "server" => server(url).await?,
+        [_, mode, url]             if mode == "client" => {
+            client(url, tokio::io::stdin()).await?
+        }
+        [_, mode, url, input_file] if mode == "client" => {
+            match tokio::fs::File::open(input_file).await {
+                Ok(file) => client(url, file).await?,
+                Err(err) => {
+                    eprintln!("Failed to open inpu_file: \"{}\", error: {}", input_file, err);
+                    process::exit(2);
+                }
+            }
+        }
+        _ => {
+            eprintln!("Usage:\n{0} server <url>\n  or\n{0} client <url> [input_file]", args[0]);
+            process::exit(1);
+        }
+    }
 }
