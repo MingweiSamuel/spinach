8d8b9776b73f352d55b4115b84096e22d23c8d7a
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-06-22 15:24:22 -0700

    Fix echo server example (very simple now)

--- examples/echo/src/main.rs
+++ examples/echo/src/main.rs
@@ -2,63 +2,55 @@
 
 use std::{env, process};
 
-// use spinach::bytes::Bytes;
 use spinach::tokio;
-// use spinach::tokio::net::TcpStream;
+use spinach::tokio::net::TcpStream;
 
-// use spinach::comp::{CompExt};
-// use spinach::hide::Hide;
-// use spinach::lattice::set_union::SetUnionRepr;
-// use spinach::lattice::bytes::BytesRepr;
-// use spinach::op::{OpExt, ReadOp, TcpOp, TcpServerOp};
-// use spinach::tcp_server::TcpServer;
-// use spinach::tag;
+use spinach::comp::{CompExt};
+use spinach::lattice::set_union::SetUnionRepr;
+use spinach::op::{OpExt, ReadOp, TcpOp, TcpServerOp};
+use spinach::tcp_server::TcpServer;
+use spinach::tag;
 
-// type WireLatRepr = BytesRepr<SetUnionRepr<tag::SINGLE, String>>;
+type WireLatRepr = SetUnionRepr<tag::SINGLE, String>;
 
-// /// Run the server portion of the program.
-// async fn server(url: &str) -> Result<!, String> {
+/// Run the server portion of the program.
+async fn server(url: &str) -> Result<!, String> {
 
-//     let server = TcpServer::bind(url).await.map_err(|e| e.to_string())?;
+    let server = TcpServer::bind(url).await.map_err(|e| e.to_string())?;
 
-//     // SetUnion<(SocketAddr, Bytes)>
-//     TcpServerOp::<WireLatRepr>::new(server.clone())
-//         .debug("server")
-//         .morphism_closure(|item| Hide::new(item.into_reveal().0.try_into().unwrap()))
-//         .morphism_closure(|item| item.map_one(|(addr, bytes)| (addr, bytes.freeze())))
-//         // TODO CONVERT: MapUnion<SocketAddr, SetUnion<Bytes>>
-//         .comp_tcp_server(server)
-//         .run()
-//         .await
-//         .map_err(|e| e.to_string())?;
-// }
+    // SetUnion<(SocketAddr, Bytes)>
+    TcpServerOp::<WireLatRepr>::new(server.clone())
+        .debug("server")
+        .comp_tcp_server(server)
+        .run()
+        .await
+        .map_err(|e| e.to_string())?;
+}
 
-// /// Run the client portion of the program.
-// async fn client<R: tokio::io::AsyncRead + std::marker::Unpin>(url: &str, input_read: R) -> Result<!, String> {
+/// Run the client portion of the program.
+async fn client<R: tokio::io::AsyncRead + std::marker::Unpin>(url: &str, input_read: R) -> Result<!, String> {
 
-//     let (read, write) = TcpStream::connect(url).await.map_err(|e| e.to_string())?
-//         .into_split();
+    let (read, write) = TcpStream::connect(url).await.map_err(|e| e.to_string())?
+        .into_split();
 
-//     let read_comp = TcpOp::<WireLatRepr>::new(read)
-//         .comp_debug("read");
+    let read_comp = TcpOp::<WireLatRepr>::new(read)
+        .comp_debug("read");
 
-//     let write_comp = ReadOp::new(input_read)
-//         .morphism_closure(|item| item.map_one::<Bytes, _>(|string| string.into()))
-//         .morphism_closure(|item| Hide::<_, WireLatRepr>::new(item.into_reveal().0)) // TODO reveal.
-//         .comp_tcp(write);
+    let write_comp = ReadOp::new(input_read)
+        .comp_tcp(write);
 
-//     let result = tokio::join!(
-//         async {
-//             read_comp.run().await.map_err(|_| format!("Read failed."))
-//         },
-//         async {
-//             write_comp.run().await.map_err(|e| e.to_string())
-//         },
-//     );
+    let result = tokio::join!(
+        async {
+            read_comp.run().await.map_err(|_| format!("Read failed."))
+        },
+        async {
+            write_comp.run().await.map_err(|e| e.to_string())
+        },
+    );
 
-//     Err(format!("Read error: {:?}, Write error: {:?}",
-//         result.0.unwrap_err(), result.1.unwrap_err()))
-// }
+    Err(format!("Read error: {:?}, Write error: {:?}",
+        result.0.unwrap_err(), result.1.unwrap_err()))
+}
 
 /// Entry point of the application.
 #[tokio::main(flavor = "current_thread")]
@@ -68,17 +60,17 @@ async fn main() -> Result<!, String> {
     let args: Vec<_> = env::args().collect();
 
     match &*args {
-        // [_, mode, url]             if mode == "server" => server(url).await?,
-        // [_, mode, url]             if mode == "client" => client(url, tokio::io::stdin()).await?,
-        // [_, mode, url, input_file] if mode == "client" => {
-        //     match tokio::fs::File::open(input_file).await {
-        //         Ok(file) => client(url, file).await?,
-        //         Err(err) => {
-        //             eprintln!("Failed to open input_file: \"{}\", error: {}", input_file, err);
-        //             process::exit(2);
-        //         }
-        //     }
-        // }
+        [_, mode, url]             if mode == "server" => server(url).await?,
+        [_, mode, url]             if mode == "client" => client(url, tokio::io::stdin()).await?,
+        [_, mode, url, input_file] if mode == "client" => {
+            match tokio::fs::File::open(input_file).await {
+                Ok(file) => client(url, file).await?,
+                Err(err) => {
+                    eprintln!("Failed to open input_file: \"{}\", error: {}", input_file, err);
+                    process::exit(2);
+                }
+            }
+        }
         _ => {
             eprintln!("Usage:\n{0} server <url>\n  or\n{0} client <url> [input_file]", args[0]);
             process::exit(1);
